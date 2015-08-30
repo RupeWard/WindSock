c2021cecf0743ae79c2ba676d55c954af23d4772
Rupe-Rosicky <rupe@rupertward.co.uk>
2015-08-30 16:16:54 +0100

    Refactored texture offset stuff into windcone, and made it depend on windspeed.

--- Assets/Scripts/AppManager.cs
+++ Assets/Scripts/AppManager.cs
@@ -1,44 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class AppManager : SingletonApplicationLifetime<AppManager> {
-
-	public Transform cameraHolder;
-
-	public MeshRenderer[] xrendrrrs = new MeshRenderer[0];
-	public MeshRenderer[] yrendrrrs = new MeshRenderer[0];
-	public float speed = 0f;
-	public float phase = 0f;
-
+public class AppManager : SingletonApplicationLifetime<AppManager> 
+{
 
 	public void OnCloseClicked()
 	{
 		Application.Quit ();
 	}
 	
-	// Update is called once per frame
-	void Update () 
-	{
-		phase += speed * Time.deltaTime;
-		while (phase > 1f) 
-		{
-			phase -= 1f;
-		}
-		foreach (MeshRenderer rendrrr in yrendrrrs) 
-		{
-			if (rendrrr != null) {
-				//			float p = rendrrr.material.GetTextureOffset("_MainTex").x;
-				rendrrr.material.SetTextureOffset("_MainTex",new Vector2(0f,1f-phase));
-			}
-		}
-		foreach (MeshRenderer rendrrr in xrendrrrs) 
-		{
-			if (rendrrr != null) {
-				//			float p = rendrrr.material.GetTextureOffset("_MainTex").x;
-				rendrrr.material.SetTextureOffset("_MainTex",new Vector2(1f-phase,0f));
-			}
-		}
-
-
-	}
 }
--- Assets/Scripts/WindCone.cs
+++ Assets/Scripts/WindCone.cs
@@ -3,11 +3,26 @@ using System.Collections;
 
 public class WindCone : MonoBehaviour 
 {
+	public float speed = 0f;
+	public float phase = 0f;
+	public MeshRenderer meshRenderer;
+
+	public bool doX = false;
+	public bool doY = false;
+
+	public float speedFactor = 1f;
+
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+	{
+		WindManager.Instance.speedChangedAction += HandleWindSpeedChange;
 	}
-	
+
+	void HandleWindSpeedChange(float s)
+	{
+		speed = speedFactor * s;
+	}
+
 	// Update is called once per frame
 	void FixedUpdate () 
 	{
@@ -16,4 +31,30 @@ public class WindCone : MonoBehaviour
 			rigidbody.AddForce (WindManager.Instance.CurrentWindVelocity);		
 		}
 	}
+
+
+	// Update is called once per frame
+	void Update () 
+	{
+		phase += speed * Time.deltaTime;
+		while (phase > 1f) 
+		{
+			phase -= 1f;
+		}
+		Vector2 offset = Vector2.zero;
+		if (doX)
+		{
+			offset.x = 1f-phase;
+		}
+		if (doY)
+		{
+			offset.y = 1f-phase;
+		}
+//		if (offset.magnitude != 0f) 
+		{
+			meshRenderer.material.SetTextureOffset("_MainTex",offset);
+		}
+		
+	}
+
 }
