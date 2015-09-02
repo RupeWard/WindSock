4f2c7fdcf87b31f947c7001a042533ee1c52efd1
Rupe-Rosicky <rupe@rupertward.co.uk>
2015-09-02 23:58:15 +0100

    Leaf spawning

---
+++ Assets/Scripts/Leaf.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Leaf : MonoBehaviour 
+{
+
+	void Start () 
+	{
+	
+	}
+	
+	void Update () 
+	{
+		if (transform.position.y < -0.5f) 
+		{
+			GameObject.Destroy(gameObject);
+		}
+	}
+
+	void FixedUpdate()
+	{
+		rigidbody.AddForce (WindManager.Instance.CurrentWindVelocity);		
+	}
+}
---
+++ Assets/Scripts/LeafSpawner.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeafSpawner : MonoBehaviour 
+{
+	public float forceMult = 100f;
+	public static float sForceMult;
+
+	public GameObject leafPrefab;
+
+	public Vector2 frequencyRange =  new Vector2(1f, 5f);
+
+	void Start () 
+	{
+		StartCoroutine (SpawnCR ());
+	}
+	
+	void Update () 
+	{
+		sForceMult = forceMult;
+	
+	}
+
+	private int leafNum = 0;
+	private IEnumerator SpawnCR()
+	{
+		float wait = UnityEngine.Random.Range (frequencyRange.x, frequencyRange.y);
+		yield return new WaitForSeconds(wait);
+
+		GameObject go = Instantiate (leafPrefab) as GameObject;
+		Leaf leaf = go.GetComponent< Leaf > ();
+		go.transform.position = transform.position;
+		go.name = "Leaf_" + leafNum;
+		leafNum++;
+
+		StartCoroutine (SpawnCR ());
+	}
+}
