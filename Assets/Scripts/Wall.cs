a9e40c185fadf35bf941d026c111a9b69daf14fe
Rupe-Rosicky <rupe@rupertward.co.uk>
2015-09-06 17:19:16 +0100

    Wall script, wind zone

---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wall : MonoBehaviour 
+{
+
+	void Start () 
+	{
+	
+	}
+	
+	void Update () 
+	{
+	
+	}
+
+	public void OnTriggerEnter(Collider col)
+	{
+		Debug.Log (gameObject.name + " OnTriggerEnter " + col.gameObject.name);
+	}
+	public void OnTriggerExit(Collider col)
+	{
+		Debug.Log (gameObject.name + " OnTriggerExit " + col.gameObject.name);
+	}
+}
