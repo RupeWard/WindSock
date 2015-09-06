2cc45dd0a777c6d71e36a3fb1982f3bbefaf5b28
Rupe-Rosicky <rupe@rupertward.co.uk>
2015-09-06 17:36:05 +0100

    Particle emitters turning on with the wind

--- Assets/Scripts/Wall.cs
+++ Assets/Scripts/Wall.cs
@@ -3,15 +3,72 @@ using System.Collections;
 
 public class Wall : MonoBehaviour 
 {
+	public GameObject partSystem;
+
+	public enum ActiveWhen
+	{
+		xPos,
+		zPos,
+		xNeg,
+		zNeg
+	}
+
+	public ActiveWhen activeWhen;
 
 	void Start () 
 	{
-	
+		WindManager.Instance.velocityChangedAction += HandleWindVelocity;
+		HandleWindVelocity (WindManager.Instance.CurrentWindVelocity);
 	}
 	
 	void Update () 
 	{
 	
+	}
+
+	public void HandleWindVelocity(Vector3 v)
+	{
+		// for wall b, want to set active when v.x > 0f
+		// wall b at -10 7 0, rot 0 270 0
+		// for wall a, want to set active when v.x < 0f
+		// 
+		bool active = false;
+		switch (activeWhen) 
+		{
+			case ActiveWhen.xPos:
+			{
+				if (v.x > 0f) 
+				{
+					active = true;
+				}
+				break;
+			}
+			case ActiveWhen.xNeg:
+			{
+				if (v.x < 0f) 
+				{
+					active = true;
+				}
+				break;
+			}
+			case ActiveWhen.zPos:
+			{
+				if (v.z > 0f) 
+				{
+					active = true;
+				}
+				break;
+			}
+			case ActiveWhen.zNeg:
+			{
+				if (v.z < 0f) 
+				{	
+					active = true;
+				}
+				break;
+			}
+		}
+		partSystem.SetActive(active);
 	}
 
 	public void OnTriggerEnter(Collider col)
