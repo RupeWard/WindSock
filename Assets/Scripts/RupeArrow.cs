1667e720fce8be02d0cfe2b12e9a1f7c3a5b9c85
Rupe-Rosicky <rupe@rupertward.co.uk>
2015-10-09 01:01:27 +0100

    WindArrow for StickSports

---
+++ Assets/Scripts/RupeArrow.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RupeArrow : MonoBehaviour 
+{
+	public float speed = 0f;
+	public float phase = 0f;
+	public MeshRenderer meshRenderer;
+
+	public bool doX = false;
+	public bool doY = false;
+
+	public float speedFactor = 1f;
+
+	public Transform cameraTransform;
+
+	public Transform myLight;
+
+	// Use this for initialization
+	void Start () 
+	{
+	}
+
+	void HandleWindSpeedChange(float s)
+	{
+		speed = speedFactor * s;
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		phase += speed * Time.deltaTime;
+		while (phase > 1f) 
+		{
+			phase -= 1f;
+		}
+		Vector2 offset = Vector2.zero;
+		if (doX)
+		{
+			offset.x = 1f-phase;
+		}
+		if (doY)
+		{
+			offset.y = 1f-phase;
+		}
+//		if (offset.magnitude != 0f) 
+		{
+			meshRenderer.material.SetTextureOffset("_MainTex",offset);
+		}
+
+		meshRenderer.material.SetFloat ("_AlphaMin", 1f);
+		meshRenderer.material.SetFloat ("_AlphaMax", 1f);
+
+	}
+
+}
